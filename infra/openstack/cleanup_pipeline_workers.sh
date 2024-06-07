ce3935db612d1e33a2026db8ee8daef69723e434
tikitavi <rtikitavi@gmail.com>
2024-06-07 17:50:00 +0300

    remove untagged instances elder than 1 hour
    Change-Id: Ibf3add755f3b6eb74ea11da5cef76fb7f76939ae
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

--- infra/openstack/cleanup_pipeline_workers.sh
+++ infra/openstack/cleanup_pipeline_workers.sh
@@ -31,3 +31,22 @@ if TERMINATION_LIST=$(list_instances PipelineBuildTag=${PIPELINE_BUILD_TAG}) ; t
     openstack volume delete $volumes
   fi
 fi
+
+# delete untagged instances elder than 1 hour
+MAX_DURATION=3600
+# collect all unlocked instances without tags with names ending on number
+TERMINATION_LIST=$(nova list --field locked,name --not-tags-any "SLAVE=openstack" | grep 'False' |  grep -E '[0-9]\s+[|]$' | tr -d '|' | awk '{print $1}')
+C_DATE=$(date +%s)
+for i in $TERMINATION_LIST; do
+  echo "INFO: Test untagged instance for duration: $i"
+  L_DATE=$(date --date $(nova show $i | grep 'OS-SRV-USG:launched_at' | tr -d '|' | awk '{print $NF}') +%s)
+  DURATION=$(($C_DATE - $L_DATE))
+  if [[ "$DURATION" -ge "$MAX_DURATION" ]]; then
+    EXCEED+="$i "
+  fi
+done
+if [[ "${#EXCEED[*]}" == "0" ]]; then
+  exit
+fi
+echo "INFO: Exceed list: ${#EXCEED[@]}"
+openstack server delete $EXCEED

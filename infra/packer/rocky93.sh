cf55f39ed4474e6bbe0bb5f12fcea0894d2f2a53
tikitavi <rtikitavi@gmail.com>
2026-02-20 20:33:41 +0300

    update cloud-init for rocky93
    Change-Id: I739e3ff78849d4d79f0c5cab60a21bed802c6da0
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

--- infra/packer/rocky93.sh
+++ infra/packer/rocky93.sh
@@ -4,3 +4,7 @@ set -o pipefail
 # NOTE: do not run dnf update - it ups kernel version which is not suported !
 
 sudo dnf install -y bind bind-utils haproxy httpd net-tools
+
+# cloud-init 23.1.1 on Rocky 9.3 fails to generate SSH host keys on first boot,
+# causing sshd to fail. Update cloud-init to fix this.
+sudo dnf update -y cloud-init
